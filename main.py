abc46c6351e3608c38e392c5a6371710795dbfef
xsi59 <sisadova@yandex.by>
2021-11-01 11:54:16 +0300

    отдельная функция recursion() для варианта 1

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-import time  # to calculate the algorithm execution time
+import time
 
 
-def fibonacci_ver1(n:int) ->int:
+def fibonacci_ver1(n: int) -> int:
     """ версия 1 Рекурсивная функция расчета n-го числа Фибоначчи
+    :rtype: object
     :param n: номер числа, которое нужно вычислить. Целое число от 0 до + бесконечности
     f(20) считалось 0.01с , f(40)  считалось 95с , f(42)  считалось 243с
     """
-    assert  n >= 0, "Ожидается целое положительное число или О"
-
-    if n <= 1:             # крайний случай: для f(0)=0, f(1)=1
+    assert n >= 0, 'Ожидается целое положительное число или О'
+    if n <= 1:  # крайний случай: для f(0)=0, f(1)=1
         return n
-    return fibonacci_ver1(n - 1) + fibonacci_ver1(n - 2)         # реккурентная формула
+    return fibonacci_ver1(n - 1) + fibonacci_ver1(n - 2)  # реккурентная формула
 
-def main():
+
+def recursion():
     """Реализация различных алгоритмов для обучения подрастающего поколения"""
     n = int(input("Введите номер числа Фибоначчи для вычисления: "))
-    start_time = time.perf_counter()        # засекаем время старта
+    start_time = time.perf_counter()  # засекаем время старта
     fib_number = fibonacci_ver1(n)
-    finish_time = time.perf_counter()        # засекаем время финиша
-    print("Расчетное число=", fib_number, "Время выполнения=",finish_time-start_time, sep=":")
+    finish_time = time.perf_counter()  # засекаем время финиша
+    print("Расчетное число=", fib_number, "Время выполнения=", finish_time - start_time, sep=":")
+
+
+def main():
+    recursion()
+
 
 if __name__ == '__main__':
     main()
