acfbc37293ed7c1a3d5a9d51d763aee74b445654
xsi59 <sisadova@yandex.by>
2021-11-01 16:38:56 +0300

    добавил модуль и логику для расчета факториала

---
+++ factorial.py
@@ -0,0 +1,6 @@
+def factorial_via_recursion(n: int):
+    pass
+
+
+def factorial_through_dynamic_programming(n: int):
+    pass
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import time
 import fibonacci as fb
+import factorial as fc
 
-algorithm_selection = int(input("Для выбора алгоритмов вычисления чисел Фибоначчи введите 1: "))
-assert ( 0 < algorithm_selection < 2),  'ожидалось целое число в диапазоне от 1 до 1'
+
+print(" Для выбора алгоритмов вычисления чисел Фибоначчи введите  1:\n",
+      "Для выбора алгоритмов вычисления факториала числа введите 2:\n",)
+algorithm_selection = int(input("Жду ввода:"))
+assert ( 0 < algorithm_selection < 3),  'ожидалось целое число в диапазоне от 1 до 2'
 
 if algorithm_selection==1:
     n = int(input("Введите номер числа Фибоначчи для вычисления: "))
-    assert (0 <= n < 10001),  'ожидалось целое число в диапазоне от 0 до 997'
+    assert (0 <= n < 997),  'ожидалось целое число в диапазоне от 0 до 997'
+elif algorithm_selection==2:
+    n = int(input("Введите число для вычисления факториала: "))
+    assert (0 <= n < 997),  'ожидалось целое число в диапазоне от 0 до 997'
 
 
 
@@ -17,8 +24,12 @@ def main():
             fib_number = foo(n)
             finish_time = time.perf_counter()
             print("Расчетное число=", fib_number, "Время выполнения=", finish_time - start_time, sep=":")
-
-
+    elif algorithm_selection == 2:
+        for  foo  in [fc.factorial_via_recursion,fc.factorial_through_dynamic_programming]:
+            start_time = time.perf_counter()
+            fib_number = foo(n)
+            finish_time = time.perf_counter()
+            print("Расчетное число=", fib_number, "Время выполнения=", finish_time - start_time, sep=":")
 
 if __name__ == '__main__':
     main()
