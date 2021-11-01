b1ba3d8170042da78b7a997b685bd613c4344ed4
xsi59 <sisadova@yandex.by>
2021-11-01 16:20:44 +0300

    расчет чисел фибоначчи вынес в отдельный модуль, добавил выбор алгоритмов

--- main.py
+++ main.py
@@ -1,54 +1,22 @@
 import time
+import fibonacci as fb
 
+algorithm_selection = int(input("Для выбора алгоритмов вычисления чисел Фибоначчи введите 1: "))
+assert ( 0 < algorithm_selection < 2),  'ожидалось целое число в диапазоне от 1 до 1'
 
-n = int(input("Введите номер числа Фибоначчи для вычисления: "))
-assert (0 <= n < 10001),  'ожидалось целое число в диапазоне от 0 до 997'
-fib = [None] * 997
+if algorithm_selection==1:
+    n = int(input("Введите номер числа Фибоначчи для вычисления: "))
+    assert (0 <= n < 10001),  'ожидалось целое число в диапазоне от 0 до 997'
 
-
-def fibonacci_ver1(n: int):
-    """ версия 1 Рекурсивная функция расчета n-го числа Фибоначчи
-    Реально вычисляет до 42 числа и скорость падает логарифмически
-    f(20) считалось 0.01с , f(40)  считалось 95с , f(42)  считалось 243с
-    """
-    if n <= 1:  # крайний случай: для f(0)=0, f(1)=1
-        return n
-    return fibonacci_ver1(n - 1) + fibonacci_ver1(n - 2)  # реккурентная формула
-
-
-def fibonacci_ver2(n: int):
-    """ версия 2 применение методов динамического программирования для расчета n-го числа Фибоначчи
-    вычисляет очень большие числа с очень большой скоростью
-    f(20) считалось 4.13с , f(40)  считалось 5.6с , f(42)  считалось 5.23с
-    """
-    fib = [None] * (n + 1)
-    fib[:2] = [0, 1]
-    for k in range(2, n+1):
-        fib[k] = fib[k - 1] + fib[k - 2]
-    return fib[k]
-
-def fibonacci_ver3(n: int):
-    """ версия 3 Рекурсивная функция расчета n-го числа Фибоначчи с кэшированием в список
-    начиная где-то с 50-го числа скорость логарифмически растет и обгоняет второй, но орграничено глубиной 996
-    f(20) считалось 3.41с , f(40)  считалось 4.1с , f(42)  считалось 9.4с
-    """
-
-    if fib[n] is not None:  #крайний случай
-        return fib[n]
-    elif n < 2:
-        fib[n] = n
-        return fib[n]
-    else:
-        fib[n] = fibonacci_ver3(n - 1) + fibonacci_ver3(n - 2)
-        return fib[n]
 
 
 def main():
-    for  foo  in [fibonacci_ver1,fibonacci_ver2,fibonacci_ver3]:
-        start_time = time.perf_counter()
-        fib_number = foo(n)
-        finish_time = time.perf_counter()
-        print("Расчетное число=", fib_number, "Время выполнения=", finish_time - start_time, sep=":")
+    if algorithm_selection == 1:
+        for  foo  in [fb.fibonacci_ver1,fb.fibonacci_ver2,fb.fibonacci_ver3]:
+            start_time = time.perf_counter()
+            fib_number = foo(n)
+            finish_time = time.perf_counter()
+            print("Расчетное число=", fib_number, "Время выполнения=", finish_time - start_time, sep=":")
 
 
 
